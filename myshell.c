8eca28397fa145ae7b7413e4129587d375ed38ca
pjd10 <pjd10@students.uwf.edu>
2015-09-16 17:25:25 -0500

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -7,38 +7,111 @@
  *  @bug    No known bugs
  */
 
+ // Library Includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <wait.h>
+
+// Parse Include
 #include "parse.h"
 
+/** @brief myshell entry point.
+ *  
+ *  This is the entry point for myshell where
+ *	we will prompt the user to enter any command
+ *  tokenize the input create a new process and 
+ *	eixexecute the command based on the argument of the
+ *	command.
+ *
+ * @return 	will return 0 upon successful completion of
+ *			the program.
+ */
 int main(int argc, char *argv[])
 {
-	/*Maximum characters in entry*/
-	char entry[256];
-	/*Creating the new structure*/
-    Param_t *param = NULL;
-    param = newParam();
-	/*Runs the program until exit is entered*/
-	while(strcmp(entry, "exit") != 0)
+	// Buffer to Hold User Input
+	char buffer[BUFFSZE];
+	
+	// Create a new Parameter Structure
+    Param_t command = {NULL, NULL, 0, 0};
+	
+	int exitStatus = 0;
+	
+	// Myshell Command Loop (Terminates when 'exit' is entered)
+	while(strcmp(buffer, "exit") != 0)
 	{
-		/*prints the prompt to the screen*/
+		int foundError = 0;
+		
+		// Setup for Forking
+		pid_t childPID;
+		
+		// Print the Prompt
 		printf ("$$$ ");
-		/*gets user entry from the command line*/
-		fgets(entry, 256, stdin);
-		/*calls the function to break the entry into tokens*/
-		tokenizer(entry, param);
-		/*checks to see if the program was booted in debug mode*/
+		
+		// Grab the Command User Input
+		fgets(buffer, BUFFSZE, stdin);
+		
+		// Breaks the Input into Tokens
+		foundError = tokenizer(buffer, &command);
+		
+		// Tokenizer Returned an Error Get a New Command
+		if(foundError != 0)
+		{
+			fprintf(stderr,"myshell: Format: command [argument1] [argument2] [<inputfile] [>outputfile] [&]\n");
+			setToNull(&command);
+			continue;
+		}
+		
+		// Is Program in Debug Mode?
 		if (argv[1] != NULL)
 		{
-			/*Prints the structure to the screen*/		
+			// Prints the Structure
 			if (strcmp(argv[1],"-Debug") == 0)
-				printParams(param);
+				printParams(&command);
 		}
-	/*Sets the structure back to default*/
-	setToNull(param);
+	
+		// Exit Before Forking Again
+		if( strcmp(command.argumentVector[0], "exit") == 0)
+		{
+			exitStatus = waitOnChildren();
+			//if children terminated successfully
+			if (exitStatus == 0){
+				setToNull(&command);
+				return printError(1,2);
+			}
+			//If there was an error with child termination
+			else{
+				// Resets the Structure
+				setToNull(&command);
+				return printError(1,1)
+			}
+
+		}
+		
+		// Fork
+		childPID = fork();
+		
+		// Forked Successfully?
+		if(childPID >= 0)
+		{
+			// Child Process
+			if(childPID == 0)
+				executeCommand(&command);
+			// Parent Process
+			else
+			{
+				// Foreground Process
+				if(command.background != 1)
+					waitpid(childPID, NULL, 0);
+			}
+		}
+		// Fork Has Failed. Terminate
+		else 
+			return printError(1, 0);
+		
+		// Resets the Structure
+		setToNull(&command);
 	}
-	/*Frees the allocated data for the struct*/
-	free(param);
 	return 0;
 }
